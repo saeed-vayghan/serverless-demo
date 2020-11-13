1fe104011f9636a425f70157b2d08eb843aea286
Saeed Vayghani <saeed.uni68@gmail.com>
2020-11-13 19:37:40 +0330

    Update express.core.js

--- config/express.core.js
+++ config/express.core.js
@@ -6,10 +6,12 @@ const app        = express();
 // Might be used later
 // const bodyParser = require('body-parser');
 // const helmet = require('helmet');
+// const hsts = require('hsts');
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(helmet());
+// app.use(hsts({ maxAge: 15552000 })); // 180 days in seconds
 
 app.disable('x-powered-by');
 app.set('showStackError', true);
